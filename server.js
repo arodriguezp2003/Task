c400aa8a3cb926178f3ae6159be8ae8cb2834176
= <=>
2014-05-05 18:28:01 -0400

    server 8181

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/', home);
 app.get('/usuarios/:id', home);
 
 
-server.listen(81);  
+server.listen(8181);  
 
 //**************** MONGO DB **************************
 mongoose.connect('mongodb://localhost/tkconnect',function(err){
